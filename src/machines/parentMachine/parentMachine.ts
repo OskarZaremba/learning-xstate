d475f0121a344048ea46f9ffdbcc8453a8602271
Oskar Zaremba <oskar.zaremba@appunite.com>
2023-10-21 13:20:19 +0200

    conditional branching

--- src/machines/parentMachine/parentMachine.ts
+++ src/machines/parentMachine/parentMachine.ts
@@ -1,23 +1,38 @@
 import { createMachine } from 'xstate';
 import { childMachine } from '../childMachine/childMachine';
 
-export const parentMachine = createMachine({
-	predictableActionArguments: true,
-	initial: 'parentMachineState1',
-	states: {
-		parentMachineState1: {
-			on: { GO_TO_PARENT_MACHINE_STATE2: 'parentMachineState2' },
+export const parentMachine = createMachine(
+	{
+		predictableActionArguments: true,
+		initial: 'parentMachineState1',
+		context: {
+			isLoggedIn: false,
 		},
-		parentMachineState2: {
-			invoke: {
-				id: 'childMachine',
-				src: childMachine,
-				onDone: 'parentMachineState3',
+		states: {
+			parentMachineState1: {
+				on: {
+					GO_TO_PARENT_MACHINE_STATE2: [
+						{ cond: 'isLoggedIn', target: 'parentMachineState3' },
+						{ target: 'parentMachineState2' },
+					],
+				},
 			},
+			parentMachineState2: {
+				invoke: {
+					id: 'childMachine',
+					src: childMachine,
+					onDone: 'parentMachineState3',
+				},
+			},
+			parentMachineState3: {
+				on: { GO_TO_PARENT_MACHINE_STATE4: 'parentMachineState4' },
+			},
+			parentMachineState4: { type: 'final' },
 		},
-		parentMachineState3: {
-			on: { GO_TO_PARENT_MACHINE_STATE4: 'parentMachineState4' },
-		},
-		parentMachineState4: { type: 'final' },
 	},
-});
+	{
+		guards: {
+			isLoggedIn: (context) => context.isLoggedIn === true,
+		},
+	}
+);
